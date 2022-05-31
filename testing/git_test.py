7be22866f897222b4fc392a43e7be71589e1238c
usr-slm <usr-slm>
2022-05-31 07:51:55 +0000

    initial submission

---
+++ testing/git_test.py
@@ -0,0 +1,10 @@
+# Databricks notebook source
+# MAGIC %run "./git_test2"
+
+# COMMAND ----------
+
+print("This is a tester.")
+
+# COMMAND ----------
+
+print(git_test2)
---
+++ testing/git_test2.py
@@ -0,0 +1,2 @@
+# Databricks notebook source
+test_var = "This is a tester from git_test2"
