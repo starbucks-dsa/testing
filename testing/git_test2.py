c75ad6f83d207222326a971ae51ec44957c5259c
brianhudnall@starbucks.co.jp <brianhudnall@starbucks.co.jp>
2022-06-01 01:12:00 +0000

    Adding in query commit
    Testing out table versioning for pull requests

--- testing/git_test2.py
+++ testing/git_test2.py
@@ -4,3 +4,9 @@ test_var = "This is a tester from git_test2"
 # COMMAND ----------
 
 test_branch = "This is a tester from dev branch"
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC select * from dsa_yanyan.temp_table;
